ff09767f8b3a66ee691e0c7695a3220e8bbd95d4
be-tho <yiyevp@gmail.com>
2023-09-28 13:59:15 -0300

    update fixed env.

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+{{--    <link rel="stylesheet" href="{{ asset('css/app.css') }}">--}}
     <title>Jr2-app | @yield('title', 'Bienvenido')</title>
 </head>
 @yield('header')
